4be87f6677f85b7af4a21852955cac0b0bfe63f7
wilbertCeiba <39089166+wilbertCeiba@users.noreply.github.com>
2020-09-14 18:02:07 -0500

    Update step5-verify.sh

--- git/committing-files/step5-verify.sh
+++ git/committing-files/step5-verify.sh
@@ -1,2 +1,1 @@
-cat .gitignore | grep -i '* .tmp' && echo "done"
-git status | grep -i 'nothing to commit, working tree clean' && echo "done"
+cat .gitignore | grep -i '* .tmp' && echo "done" && git status | grep -i 'nothing to commit, working tree clean' && echo "done"
