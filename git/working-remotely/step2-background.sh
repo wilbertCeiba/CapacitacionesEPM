c684cb7ef1a9b0756ecb374fa6926b06e0de5a09
Wilbert Yesith Bermeo Meneses <wilbert.bermeo@ceiba.com.co>
2020-09-24 11:10:00 -0500

    prueba

--- git/working-remotely/step2-background.sh
+++ git/working-remotely/step2-background.sh
@@ -1,5 +1,5 @@
 cd Aprendiendo_Git
 $Number = echo $((1 + RANDOM % 10))
-echo 'Área de preparación' > staging_$Number.txt
-git add staging_$Number.txt
+echo 'Área de preparación' > "staging_$Number.txt"
+git add "staging_$Number.txt"
 git commit -m "Initial Commit"
