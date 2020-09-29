39377c8fd7d759a3d7c545aa53fcb1202bda78eb
Wilbert Yesith Bermeo Meneses <wilbert.bermeo@ceiba.com.co>
2020-09-29 16:12:43 -0500

    se modifica background de escenario 6

--- git/fixing-merge-conflicts/intro-background.sh
+++ git/fixing-merge-conflicts/intro-background.sh
@@ -1,10 +1,12 @@
 git clone https://capacitaciones-azure-devops@dev.azure.com/capacitaciones-azure-devops/Aprendiendo_Git/_git/Aprendiendo_Git
+sleep 10
 cd Aprendiendo_Git
 file="merge_$RANDOM.txt"
 echo 'console.log("Modificado por otro desarrollador");' > $file
 git add $file
 git commit -m "Commit Inicial"
 git push https://tb3yofvnvj2dunk5ty6xlq6574lmk66eqnbfltjazlyjwedix2kq@dev.azure.com/capacitaciones-azure-devops/Aprendiendo_Git/_git/Aprendiendo_Git master
+sleep 10
 echo 'console.log("Modificado por mi");' > $file
 git add $file
 git commit -m "Commit Secundario"
