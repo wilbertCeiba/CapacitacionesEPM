f2feab957af36446a817c7ad65138742dc9c8fe8
Wilbert Yesith Bermeo Meneses <wilbert.bermeo@ceiba.com.co>
2020-10-01 17:26:29 -0500

    test

--- git/fixing-merge-conflicts/intro-background.sh
+++ git/fixing-merge-conflicts/intro-background.sh
@@ -8,9 +8,10 @@ git checkout "feature/merge_$number_random"
 echo 'console.log("Modificado por otro desarrollador");' > $file
 git add $file
 git commit -m "Commit Inicial"
+git push https://tb3yofvnvj2dunk5ty6xlq6574lmk66eqnbfltjazlyjwedix2kq@dev.azure.com/capacitaciones-azure-devops/Aprendiendo_Git/_git/Aprendiendo_Git "feature/merge_$number_random"
 git fetch 
 git checkout remotes/origin/master
-git merge "feature/merge_$number_random"
+git merge remotes/origin/"feature/merge_$number_random"
 git push https://tb3yofvnvj2dunk5ty6xlq6574lmk66eqnbfltjazlyjwedix2kq@dev.azure.com/capacitaciones-azure-devops/Aprendiendo_Git/_git/Aprendiendo_Git remotes/origin/master
 sleep 3
 git checkout master
