bedd2159bab3b3bd0d6a945b13c195fa396e1bb7
Wilbert Yesith Bermeo Meneses <wilbert.bermeo@ceiba.com.co>
2020-10-02 18:27:58 -0500

    master

--- git/fixing-merge-conflicts/step1-foreground.sh
+++ git/fixing-merge-conflicts/step1-foreground.sh
@@ -3,19 +3,11 @@ file="merge_$number_random.txt"
 git clone https://capacitaciones-azure-devops@dev.azure.com/capacitaciones-azure-devops/Aprendiendo_Git/_git/Aprendiendo_Git
 sleep 3
 cd Aprendiendo_Git
-git branch "features/merge_$number_random"
-git checkout "features/merge_$number_random"
 echo 'console.log("Modificado por otro desarrollador");' > $file
 git add $file
 git commit -m "Commit Inicial"
-git push https://tb3yofvnvj2dunk5ty6xlq6574lmk66eqnbfltjazlyjwedix2kq@dev.azure.com/capacitaciones-azure-devops/Aprendiendo_Git/_git/Aprendiendo_Git "features/merge_$number_random"
+git push https://tb3yofvnvj2dunk5ty6xlq6574lmk66eqnbfltjazlyjwedix2kq@dev.azure.com/capacitaciones-azure-devops/Aprendiendo_Git/_git/Aprendiendo_Git master
 sleep 3
-git fetch origin
-git checkout remotes/origin/master
-git merge "features/merge_$number_random"
-git push -f https://tb3yofvnvj2dunk5ty6xlq6574lmk66eqnbfltjazlyjwedix2kq@dev.azure.com/capacitaciones-azure-devops/Aprendiendo_Git/_git/Aprendiendo_Git remotes/origin/master
-sleep 3
-git checkout master
 git checkout .
 echo 'console.log("Modificado por mi");' > $file
 git add $file
