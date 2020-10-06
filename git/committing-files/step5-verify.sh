3b5fe6d3d67ae4b0b17bec5fa98dd42e10f62e18
Wilbert Yesith Bermeo Meneses <wilbert.bermeo@ceiba.com.co>
2020-10-06 08:15:22 -0500

    test

--- git/committing-files/step5-verify.sh
+++ git/committing-files/step5-verify.sh
@@ -1,1 +1,1 @@
-cat .gitignore | grep -i '* .tmp' && echo "done" && git status | grep -i 'nothing to commit, working tree clean' && echo "done"
+git status | grep -i 'nothing to commit, working tree clean' && echo "done"
